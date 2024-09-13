acf6bb4cb5ace163aaf98153a44f4b9b3d8c8580
olivia-johnson <olj5016@e1-056309.science.psu.edu>
2024-09-13 14:16:48 -0400

    format script to be run from command line

--- gross_analysis.R
+++ gross_analysis.R
@@ -1,15 +1,21 @@
 ## GRoSS analysis ##
 
+filename <- commandArgs(trailingOnly = TRUE)
+
+
 library(data.table)
 
 setwd("~/Documents/arg_selection/")
 
-ac_files=list.files(path ="~/Documents/arg_selection/",pattern ="ac_")
-dt=fread(ac_files[1])
+dt=fread(filename)
 dt[, count:=paste0(A,",",a), by=c("pop","time","pos")]
 dt[, pop_id:=paste0("p", pop,"_",time), by=c("pop", "time")]
-grossinput=dcast(dt, pos~pop_id, value.var='count')
+grossinput=dcast(dt, pos~pop_id, value.var='count',fill = "0,0")
 grossinput[, CHROM:=1]
+grossinput[, SNPID:=pos, by="pos"]
 setnames(grossinput, 'pos', 'POS')
+setcolorder(grossinput, c("CHROM","POS","SNPID"))
 
-fwrite(grossinput, ac_files[1], sep = "\t")
+fwrite(grossinput, filename, sep = "\t")
+
+
