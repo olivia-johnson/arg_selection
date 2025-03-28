67332dead9f7ce0a1308bc854c8585135360d72a
olivia-johnson <olj5016@psu.edu>
2025-03-28 15:56:21 -0400

    Run from python and pass filename

--- pval.R
+++ pval.R
@@ -1,18 +1,22 @@
+#!/usr/bin/env Rscript
+filename <- commandArgs(trailingOnly = TRUE)
+
 library(data.table)
 library(ggplot2)
 
-setwd(setwd("~/Documents/arg_selection/")
+setwd("~/Documents/arg_selection/")
 
-dt=fread("windowedlr_single_0_s1.0_sT17000_sE20000_sP1_cF1.0_cFT17499_admix0.0_sSize100.txt")
+dt=fread(filename)
 dt[, pval:=pchisq(l_ratio, df = 1, lower.tail = FALSE)]
+dt[,ppval:=-log10(pval)]
 
 
 tdiff=ggplot(dt, aes(x=Pos, y=l_ratio))+
   geom_line() + 
   theme_bw()
-ggsave("tdiff_0_s1.0_sT17000_sE20000_sP1_cF1.0_cFT17499_admix0.0_sSize100.pdf", tdiff)
+ggsave(paste0("tdiff_",filename,".pdf"), tdiff)
 
-plot=ggplot(dt, aes(x=Pos, y=pval))+
-  geom_line() + scale_y_log10() +
+plot=ggplot(dt, aes(x=Pos, y=ppval))+
+  geom_line() + 
   theme_bw()
-ggsave("pval_s1.0_s1.0_sT17000_sE20000_sP1_cF1.0_cFT17499_admix0.0_sSize100.pdf", plot)
+ggsave(paste0("pval_",filename,".pdf"), plot)
